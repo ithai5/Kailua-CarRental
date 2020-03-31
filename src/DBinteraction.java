84b8f0e21b10b339b65758ba3304f61ef6786a44
Itai Gramse <ithai.g@gmail.com>
2020-03-31 18:49:59 +0200

    add class main

--- src/DBinteraction.java
+++ src/DBinteraction.java
@@ -1,8 +1,2 @@
-public interface DBinteraction {
-    //test
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("that shit works");
-    }
+public class DBinteraction {
 }
