cf80027698df5f5afac30051ceb97c3e9099ba02
Itai Gramse <ithai.g@gmail.com>
2020-04-03 16:02:47 +0200

    Final DB interaction

--- src/DBinteraction.java
+++ src/DBinteraction.java
@@ -1,3 +1,60 @@
-public class DBinteraction {
-    //change
+import java.sql.*;
+
+public class DBInteraction{
+    private static String user;
+    private static String password;
+    private static String address;
+
+    public DBInteraction (String user, String password,String address)
+    {
+        this.user = user;
+        this.password = password;
+        this.address = "jdbc:mysql://" + address + "/KeaProject";
+    }
+
+    public static ResultSet getData(String query)
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(address,user,password );
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            connection.close();
+            return rs;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static int updateDatabase(String query)
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(address,user,password);
+            Statement statement = connection.createStatement();
+            int  result = statement.executeUpdate(query);
+            connection.close();
+            return result;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public void setUser (String user)
+    {
+        this.user = user;
+    }
+
+    public void setPassword (String password)
+    {
+        this.password = password;
+    }
+
+    public void setAddress (String address)
+    {
+        this.address = address;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main (String[] args)
     {
         System.out.println("hello world");
+        DBInteraction db = new DBInteraction("user","password","address");
     }
 }
