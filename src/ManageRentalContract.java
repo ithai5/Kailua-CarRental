3441ae3a7b70ee7c6266817c837d92e207ffefdd
Itai Gramse <ithai.g@gmail.com>
2020-04-05 14:24:07 +0200

    add RS for available cars between dates
    taking two dates and classname and return the cars that available

--- src/ManageRentalContract.java
+++ src/ManageRentalContract.java
@@ -5,7 +5,7 @@ public class ManageRentalContract {
     //for minimize the option, can be added where to the query
     public static void viewRentalContract(String whereQuery)
     {
-        ResultSet rs = DBInteraction.getData("SELECT Customer.firstName, Customer.lastName, RentalContract.startDate, RentalContract.endDate, CarInfo.licencePlate, Specs.brand, Specs.model,ClassType.className " + "" +
+        ResultSet rs = DBinteraction.getData("SELECT Customer.firstName, Customer.lastName, RentalContract.startDate, RentalContract.endDate, CarInfo.licencePlate, Specs.brand, Specs.model,ClassType.className " + "" +
                 "FROM KeaProject.RentalContract " +
                 "INNER JOIN KeaProject.Customer ON Customer.customer_id = RentalContract.customer_id " +
                 "INNER JOIN KeaProject.CarInfo ON  RentalContract.licencePlate = CarInfo.licencePlate " +
@@ -23,5 +23,36 @@ public class ManageRentalContract {
         }
     }
 
+    //return result set of available cars between two dates
+    public static ResultSet availableCars(String startDate, String endDate, String className){
+        String query = "SELECT licencePlate, startDate, endDate,brand, model, className " +
+                "FROM KeaProject.CarInfo " +
+                "LEFT JOIN KeaProject.RentalContract USING (licencePlate) " +
+                "JOIN KeaProject .Specs USING (specs_id) " +
+                "JOIN KeaProject.ClassType USING (className_id) " +
+                "WHERE NOT startDate >= '" + startDate +"' AND endDate <= '" + endDate + "' AND className LIKE '%"+ className +"' " +
+                "UNION " +
+                "SELECT licencePlate, startDate, endDate,brand, model, className " +
+                "FROM KeaProject.CarInfo " +
+                "LEFT JOIN KeaProject.RentalContract USING (licencePlate) " +
+                "JOIN KeaProject .Specs USING (specs_id) " +
+                "JOIN KeaProject.ClassType USING (className_id) " +
+                "WHERE startDate IS NULL AND className LIKE '%"+ className +"' "+
+                "GROUP BY licencePlate";
+        ResultSet rs = DBinteraction.getData(query);
+        System.out.printf("%-8s%-17s%-15s%-15s%-15s\n","Num","Licence", "Brand", "Model","Class");
+        System.out.println("____________________________________________________________________");
+        int i = 1;
+        try{
+            while (rs.next()){
+                System.out.printf("%-8s%-17s%-15s%-15s%-15s\n","["+i+"]",rs.getString("licencePlate"),rs.getString("brand"),rs.getString("Model"),rs.getString("className"));
+                i++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } return rs;
+    }
+
+    
     
 }
