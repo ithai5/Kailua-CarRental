ff4709ecf8e449c264e3869ce924363707532c50
Thomas <thomas.anastacio@gmail.com>
2020-04-06 23:33:10 +0200

    ManageCustomer class implemented
    Functionality for Customer lies here: Create, Update, View and Delete. The rest of the program is outdated, so merge with care

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main (String[] args)
-    {
+    public Main() {
+    }
+
+    public static void customerMenu() throws SQLException {
+        System.out.println("Please choose an option\n[1]. Create new customer\n[2]. View all customers\n[3]. Update customer info\n[4]. Delete customer");
+        int choice = ScannerReader.scannerInt(1, 4);
+        switch(choice) {
+            case 1:
+                DBInteraction.updateDatabase(ManageCustomer.newCustomerQuery());
+                System.out.println("New customer has been created!");
+                break;
+            case 2:
+                ManageCustomer.printCustomerList();
+                break;
+            case 3:
+                DBInteraction.updateDatabase(ManageCustomer.updateCustomerQuery());
+                break;
+            case 4:
+                DBInteraction.updateDatabase(ManageCustomer.deleteCustomerQuery());
+        }
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, SQLException {
         System.out.println("hello world");
-        DBInteraction db = new DBInteraction("user","password","address");
+        Scanner sc = new Scanner(new File("password.txt"));
+        new DBInteraction(sc.next(), sc.next(), sc.next());
+        customerMenu();
     }
 }
---
+++ src/ManageCustomer.java
@@ -0,0 +1,99 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ManageCustomer {
+    public ManageCustomer() {
+    }
+
+    public static String quoteForString(String value, boolean withComma) {
+        return withComma ? "\"" + value + "\", " : "\"" + value + "\"";
+    }
+
+    public static String newCustomerQuery() {
+        System.out.print("Please enter the customer's first name: ");
+        String firstName = ScannerReader.scannerWords();
+        System.out.println();
+        System.out.print("Please enter the customer's last name: ");
+        String lastName = ScannerReader.scannerWords();
+        System.out.println();
+        System.out.print("Which e-mail would the customer like to be contacted on?: ");
+        String email = ScannerReader.scannerEMail();
+        System.out.println();
+        String var10000 = quoteForString(firstName, true);
+        String newCustomer = "INSERT INTO KeaProject.Customer (firstName, lastName, email)\nVALUES (" + var10000 + quoteForString(lastName, true) + quoteForString(email, false) + ");";
+        return newCustomer;
+    }
+
+    public static String updateCustomerQuery() throws SQLException {
+        System.out.println("Which customer's details would you like to change?");
+        int customerId = getCustomerById();
+        System.out.println("What information would you like to update?\n[1]. First name\n[2]. Last name\n[3]. E-mail address");
+        int choice = ScannerReader.scannerInt(1, 3);
+        String field = getField(choice);
+        System.out.print("Please input the updated information: ");
+        String changedInfo = "";
+        if (choice == 3) {
+            changedInfo = quoteForString(ScannerReader.scannerEMail(), false);
+        } else {
+            changedInfo = quoteForString(ScannerReader.scannerWords(), false);
+        }
+
+        System.out.println();
+        String updateQuery = "UPDATE Customer\nSET " + field + " = " + changedInfo + "\nWHERE Customer.customer_id = " + customerId + ";";
+        return updateQuery;
+    }
+
+    public static void printCustomerList() throws SQLException {
+        ResultSet listToPrint = DBInteraction.getData("SELECT Customer.customer_id, Customer.firstName, Customer.lastName FROM Customer");
+
+        while(listToPrint.next()) {
+            String cusId = "[" + listToPrint.getInt(1) + "].";
+            String fName = listToPrint.getString(2);
+            String lName = listToPrint.getString(3);
+            System.out.printf("%-5s%-10s%-10s", cusId, fName, lName);
+            System.out.println();
+        }
+
+    }
+
+    public static String deleteCustomerQuery() throws SQLException {
+        System.out.println("Which customer would you like to remove from the database?");
+        int customerId = getCustomerById();
+        String deleteQuery = "DELETE FROM Customer WHERE Customer.customer_id = " + customerId;
+        return deleteQuery;
+    }
+
+    public static int findCustomerId(int fieldName, String searchParam) throws SQLException {
+        String field = getField(fieldName);
+        String queryId = "SELECT Customer.customer_id\nFROM Customer\nWHERE " + field + " = \"" + searchParam + "\";";
+        ResultSet rs = DBInteraction.getData(queryId);
+        rs.next();
+        return rs.getInt(1);
+    }
+
+    public static int getCustomerById() throws SQLException {
+        printCustomerList();
+        return findCustomerId(0, "" + ScannerReader.scannerInt(1, tableSize("Customer")));
+    }
+
+    public static String getField(int fieldName) {
+        switch(fieldName) {
+            case 0:
+                return "Customer.customer_id";
+            case 1:
+                return "Customer.firstName";
+            case 2:
+                return "Customer.lastName";
+            case 3:
+                return "Customer.email";
+            default:
+                return "gvrbvnzo";
+        }
+    }
+
+    public static int tableSize(String table) throws SQLException {
+        ResultSet size = DBInteraction.getData("SELECT COUNT(*)\nFROM " + table);
+        size.next();
+        return size.getInt(1);
+    }
+}
